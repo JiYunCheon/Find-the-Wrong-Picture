5977e2c8924a8331149bba600f86a5f3fea3f20d
CJY <dkffpsdnjzj970@gmail.com>
2022-12-21 17:53:16 +0900

    GameManager Mamber, UiManaget Script fix

--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("========Ui Box========")]
+    [SerializeField] private GameObject gameOverUi = null;
+    [Header("")]
+    [SerializeField] private TextMeshProUGUI timerText = null;
+    [Header("")]
+    [SerializeField] private TextMeshProUGUI scoreText = null;
 
-    // Update is called once per frame
-    void Update()
+    public TextMeshProUGUI GetScoreText { get { return scoreText; } private set { } }
+
+    public GameObject GameOverUi { get { return gameOverUi; } private set { } }
+
+    public void SetTimerText(float curTime)
     {
-        
+        timerText.text = Mathf.RoundToInt(curTime).ToString();
+    }
+    public void CallTimeOverUi(bool active = true)
+    {
+        GameOverUi.SetActive(active);
     }
 }
