38f6d63c49f6fb625e9bc25c6e99aa12806cc27c
CJY <dkffpsdnjzj970@gmail.com>
2022-12-22 13:50:38 +0900

    add object inst lojic

---
+++ Assets/Script/Manager/SpawnManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManger : MonoBehaviour
+{
+    [SerializeField] private CreatePicture board = null;
+
+    int[] randomIndex;
+
+    void Start()
+    {
+        GenerateAnswerBoard();
+        GenerateWrongBoard();
+    }
+
+    private void GenerateAnswerBoard()
+    {
+        CreatePicture obj = Instantiate<CreatePicture>(board);
+        obj.transform.position = new Vector3(-1.05f, -0.5f, 1);
+        obj.CallChangeType();
+        randomIndex = obj.randomIndex;
+
+    }
+
+    private void GenerateWrongBoard()
+    {
+        CreatePicture obj = Instantiate<CreatePicture>(board);
+        obj.transform.position = new Vector3(1.05f, -0.5f, 1);
+        obj.GenerateWrongObject(randomIndex);
+    }
+
+}
