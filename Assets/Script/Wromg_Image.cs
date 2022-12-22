7c2adc3f3da56950a2bad51922ebf12564644680
KimTaeHoon <tibaeg7602@gamil.com>
2022-12-22 13:55:29 +0900

    틀린그림 표시

--- Assets/Script/Wromg_Image.cs
+++ Assets/Script/Wromg_Image.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class Wromg_Image : MonoBehaviour, Click_Interactable
 {
-    public bool background = false;
     public bool check { get; set; }
-    int a = 0;
+    private bool background = false;
+
+    SpriteRenderer sp = null;
+    [SerializeField] private Sprite[] default_Sprite;
+    [SerializeField] private Sprite[] wromg_Sprite;
+
+    int randomIdx = 0;
+
+    private void Awake()
+    {
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        randomIdx = Random.Range(0, default_Sprite.Length);
+        Init();
+    }
+
+    private void Init()
+    {
+        sp.sprite = default_Sprite[randomIdx];
+    }
 
     public void Interact()
     {
         Debug.Log("½ÇÇà");
         check = true;
+    }
+
+    public void On_Change_Image()
+    {
+        randomIdx = Random.Range(0, wromg_Sprite.Length);
+        sp.sprite = wromg_Sprite[randomIdx];
+        this.gameObject.layer = 9;
+    }
+
+    public void Off_Image()
+    {
+        this.gameObject.SetActive(false);
     }
 }
